35a7c24df537c08383c022272af2aa0313ab4e12
romaric <nzekeng@gmail.com>
2019-12-06 01:32:29 +0200

    Added bid optimization trending analysis and tracking source trending

---
+++ Traffic Source Analysis/Tracking_sources_trending.sql
@@ -0,0 +1,16 @@
+USE MavenFuzzyFactory
+GO 
+
+/*
+	(tracking source trending)
+	Pull gsearch nonbrand trended session volume by week 
+*/	
+
+SELECT
+	 MIN(CAST(created_at AS DATE)) WeekStartDate 
+	,COUNT(DISTINCT website_session_id) numberOfSessions
+FROM website_sessions
+WHERE created_at < '2012-05-10'
+	AND utm_source = 'gsearch'
+	AND utm_campaign = 'nonbrand'
+GROUP BY DATEPART(WEEK, created_at)
