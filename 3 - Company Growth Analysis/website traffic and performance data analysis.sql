3e5d1587570174df00a5abcc78e01945ecda10f5
romaric <nzekeng@gmail.com>
2019-12-10 00:16:05 +0200

    Added Montly trends for gsearch sessions/orders split out for nonbrand and brand campaign separately

--- 3 - Company Growth Analysis/website traffic and performance data analysis.sql
+++ 3 - Company Growth Analysis/website traffic and performance data analysis.sql
@@ -11,7 +11,7 @@ GO
 ======================================================================================================*/
 
 
---## 1- Montly trends for gseach sessions and orders so that we can show case the growth there
+--## 1- Montly trends for gsearch sessions and orders so that we can show case the growth there
 
 SELECT YEAR(ws.created_at) [year]
 	, MONTH(ws.created_at) [month]
@@ -24,4 +24,27 @@ WHERE  ws.created_at < '2012-11-27'
 	AND utm_source = 'gsearch'
 GROUP BY YEAR(ws.created_at), MONTH(ws.created_at)
 ORDER BY [year], [month];
+GO
+
+--## 2- Montly trends for gsearch sessions/orders split out for nonbrand and brand campaign separately
+
+SELECT YEAR(ws.created_at) [year]
+	, MONTH(ws.created_at) [month]
+	, COUNT(DISTINCT CASE WHEN ws.utm_campaign='nonbrand' THEN ws.website_session_id ELSE NULL END) [nonbrand_sessions]
+	, COUNT(DISTINCT CASE WHEN ws.utm_campaign='nonbrand' THEN o.order_id ELSE NULL END) [nonbrand_orders]
+	, COUNT(DISTINCT CASE WHEN ws.utm_campaign='brand' THEN ws.website_session_id ELSE NULL END) [brand_sessions]
+	, COUNT(DISTINCT CASE WHEN ws.utm_campaign='brand' THEN o.order_id ELSE NULL END) [brand_orders]
+FROM website_sessions ws
+	LEFT JOIN orders o 
+		ON o.website_session_id = ws.website_session_id
+WHERE  ws.created_at < '2012-11-27'
+	AND utm_source = 'gsearch'
+GROUP BY YEAR(ws.created_at), MONTH(ws.created_at)
+ORDER BY [year], [month];
+GO
+
+
+
+
+
 
