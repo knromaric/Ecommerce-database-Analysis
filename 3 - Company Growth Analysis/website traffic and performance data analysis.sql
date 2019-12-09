56b502dabbe86c313c47fdafe3f128bc46879998
romaric <nzekeng@gmail.com>
2019-12-09 23:53:46 +0200

    Added Company Growth analysis

---
+++ 3 - Company Growth Analysis/website traffic and performance data analysis.sql
@@ -0,0 +1,27 @@
+USE MavenFuzzyFactory;
+GO 
+
+/*====================================================================================================
+			Quantify Company growth (revenue impact) by extracting and analyzing website traffic and 
+			performance data from the Maven Fuzzy Factory database, and tell the story
+			on how you have been able to generate that growth
+
+			Analyze current performance and use the data available to assess upcoming opportunities
+
+======================================================================================================*/
+
+
+--## 1- Montly trends for gseach sessions and orders so that we can show case the growth there
+
+SELECT YEAR(ws.created_at) [year]
+	, MONTH(ws.created_at) [month]
+	, COUNT(DISTINCT ws.website_session_id) [sessions]
+	, COUNT(DISTINCT o.order_id) [orders]
+FROM website_sessions ws
+	LEFT JOIN orders o 
+		ON o.website_session_id = ws.website_session_id
+WHERE  ws.created_at < '2012-11-27'
+	AND utm_source = 'gsearch'
+GROUP BY YEAR(ws.created_at), MONTH(ws.created_at)
+ORDER BY [year], [month];
+
