1b3626fe9a17343b2895825d9e69c97289346ff9
romaric <nzekeng@gmail.com>
2019-12-10 20:16:52 +0200

    Added Estimation of the revenue that the last test earned us

--- 3 - Company Growth Analysis/website traffic and performance data analysis.sql
+++ 3 - Company Growth Analysis/website traffic and performance data analysis.sql
@@ -10,8 +10,9 @@ GO
 
 ======================================================================================================*/
 
-
+--###########################################################################################################
 --## 1- Montly trends for gsearch sessions and orders so that we can show case the growth there
+--###########################################################################################################
 
 SELECT YEAR(ws.created_at) [year]
 	, MONTH(ws.created_at) [month]
@@ -27,7 +28,9 @@ GROUP BY YEAR(ws.created_at), MONTH(ws.created_at)
 ORDER BY [year], [month];
 GO
 
+--###########################################################################################################
 --## 2- Montly trends for gsearch sessions/orders split out for nonbrand and brand campaign separately
+--###########################################################################################################
 
 SELECT YEAR(ws.created_at) [year]
 	, MONTH(ws.created_at) [month]
@@ -44,7 +47,9 @@ GROUP BY YEAR(ws.created_at), MONTH(ws.created_at)
 ORDER BY [year], [month];
 GO
 
+--###########################################################################################################
 --## 3- Let's Dive into nonbrand and pull monthly trend sessions and orders split by device type
+--###########################################################################################################
 
 SELECT YEAR(ws.created_at) [year]
 	, MONTH(ws.created_at) [month]
@@ -62,8 +67,10 @@ GROUP BY YEAR(ws.created_at), MONTH(ws.created_at)
 ORDER BY [year], [month];
 GO
 
+--###########################################################################################################
 --## 4- Montly trends for Gsearch alongside monthly trends for each of our other channels
-	
+--###########################################################################################################	
+
 --find the different utm sources and referers to see the traffic we're getting 
 
 SELECT DISTINCT	
@@ -87,8 +94,83 @@ GROUP BY YEAR(created_at), MONTH(created_at)
 ORDER BY [year], [month];
 GO
 
+--###########################################################################################################
+--## 5- Story of our website performance improvements over the course of the first 8 month(session to order conversion rates by months)
+--###########################################################################################################
 
+SELECT YEAR(ws.created_at) [year]
+	, MONTH(ws.created_at) [month]
+	, COUNT(DISTINCT ws.website_session_id) [sessions]
+	, COUNT(DISTINCT o.order_id) [orders]
+	, COUNT(DISTINCT o.order_id)*1.0 / COUNT(DISTINCT ws.website_session_id) [sessions_to_orders_rate]
+FROM website_sessions ws
+	LEFT JOIN orders o 
+		ON o.website_session_id = ws.website_session_id
+WHERE  ws.created_at < '2012-11-27'
+	AND utm_source = 'gsearch'
+GROUP BY YEAR(ws.created_at), MONTH(ws.created_at)
+ORDER BY [year], [month];
+GO
+--###########################################################################################################
+--## 6- For Gsearch lander test, estimate the revenue that the test earned us
+--###########################################################################################################
 
+-- find the pageviews_id of all the landing page per sessions
+SELECT ws.website_session_id
+	,MIN(wp.website_pageview_id) landing_page_id
+INTO #landing_page_table
+FROM website_sessions ws
+	LEFT JOIN website_pageviews wp 
+		ON ws.website_session_id = wp.website_session_id
+WHERE wp.created_at >= '2012-06-19' AND wp.created_at <= '2012-07-28'
+	AND ws.utm_source = 'gsearch'
+	AND utm_campaign = 'nonbrand'
+GROUP BY ws.website_session_id
 
+-- find the url of all the landing pages
+SELECT lpt.website_session_id 
+	,wp.pageview_url
+INTO #session_id_w_landing_page_url
+FROM #landing_page_table lpt
+	LEFT JOIN website_pageviews wp
+		ON wp.website_pageview_id = lpt.landing_page_id
+WHERE wp.pageview_url IN ('/lander-1', '/home');
 
+-- compare the sessions to orders conversion rate for lander and home pages
+SELECT slpu.pageview_url landing_page
+	, COUNT(DISTINCT slpu.website_session_id) [sessions]
+	, COUNT(DISTINCT o.order_id) [orders]
+	, COUNT(DISTINCT o.order_id)*1.0 / COUNT(DISTINCT slpu.website_session_id) sessions_orders_rate
+FROM #session_id_w_landing_page_url slpu
+	LEFT JOIN orders o 
+		ON o.website_session_id = slpu.website_session_id
+GROUP BY slpu.pageview_url;
+GO  -- .032 for /home, vs .0409 for /lander-1 -->> 0.0089 additional orders per session
+
+-- find the most recent page viewed by gsearch nonbrand where the traffic was sent to /home
+
+SELECT MAX(ws.website_session_id) most_recent_gsearch_nonbrand_home_pageview
+FROM website_sessions ws
+	LEFT JOIN website_pageviews wp 
+		ON ws.website_session_id = wp.website_session_id
+WHERE  ws.created_at < '2012-11-27'
+	AND ws.utm_source = 'gsearch'
+	AND ws.utm_campaign = 'nonbrand'
+	AND wp.pageview_url = '/home' 
+GO   
+-- result -->>  sessions = 17145
+
+-- Count of sessions since the last test
+
+SELECT COUNT(DISTINCT website_session_id) AS count_sessions_since_test
+FROM website_sessions
+WHERE created_at < '2012-11-27'
+	AND website_session_id > 17145 -- last /home session
+	AND utm_source = 'gsearch'
+	AND utm_campaign = 'nonbrand';
+GO
+-- 22450 website sessions since the last test
+
+-- 22450 * 0.0089 incremental conversion = 200 incremental orders since 7/29
+	-- roughly 4 months, so roughly 50 extra orders per month. NOT BAD!!!
 
