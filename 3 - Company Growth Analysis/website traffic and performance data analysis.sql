e56615763eed8f5c5ac1d629e0604ac85633033a
romaric <nzekeng@gmail.com>
2019-12-10 21:17:18 +0200

    Add full conversion funnel from lander and home page

--- 3 - Company Growth Analysis/website traffic and performance data analysis.sql
+++ 3 - Company Growth Analysis/website traffic and performance data analysis.sql
@@ -174,3 +174,81 @@ GO
 -- 22450 * 0.0089 incremental conversion = 200 incremental orders since 7/29
 	-- roughly 4 months, so roughly 50 extra orders per month. NOT BAD!!!
 
+
+--###############################################################################################################
+--## 7- For the landing page test we analyzed previously, it would be great to show a full conversion funnel
+--##    from each of the 2 pages to orders, we will use the same time period (june 19 - july 28).
+--###############################################################################################################
+
+SELECT website_session_id
+	, MAX(home_page) as home_made_it
+	, MAX(lander_page) as lander_made_it
+	, MAX(product_page) as products_made_it
+	, MAX(mrfuzzy_page) as mrfuzzy_made_it
+	, MAX(cart_page) as cart_made_it
+	, MAX(shipping_page) shipping_made_it
+	, MAX(billing_page) as billing_made_it
+	, MAX(thank_you_page) as thank_you_made_it
+INTO #session_level_made_if_flagged
+FROM (
+	SELECT ws.website_session_id
+		, wp.pageview_url
+		, wp.created_at AS pageviews_created_at
+		, (CASE WHEN wp.pageview_url = '/home' THEN 1 ELSE 0 END) AS home_page
+		, (CASE WHEN wp.pageview_url = '/lander-1' THEN 1 ELSE 0 END) AS lander_page
+		, (CASE WHEN wp.pageview_url = '/products' THEN 1 ELSE 0 END) AS product_page
+		, (CASE WHEN wp.pageview_url = '/the-original-mr-fuzzy' THEN 1 ELSE 0 END) AS mrfuzzy_page
+		, (CASE WHEN wp.pageview_url = '/cart' THEN 1 ELSE 0 END) AS cart_page
+		, (CASE WHEN wp.pageview_url = '/shipping' THEN 1 ELSE 0 END) AS shipping_page
+		, (CASE WHEN wp.pageview_url = '/billing' THEN 1 ELSE 0 END) AS billing_page
+		, (CASE WHEN wp.pageview_url = '/thank-you-for-your-order' THEN 1 ELSE 0 END) AS thank_you_page
+	FROM website_sessions ws
+		LEFT JOIN website_pageviews wp
+			ON ws.website_session_id = wp.website_session_id
+	WHERE wp.created_at >= '2012-06-19' AND wp.created_at <='2012-07-28'
+		AND utm_source = 'gsearch'
+		AND utm_campaign = 'nonbrand') pageview_level
+GROUP BY website_session_id
+GO
+
+SELECT 
+	CASE 
+		WHEN home_made_it = 1 THEN 'saw_homepage'
+		WHEN lander_made_it = 1 THEN 'saw_custom_lander'
+		ELSE 'check the logic '
+	END AS segment
+	, COUNT(DISTINCT website_session_id) AS sessions
+	, COUNT(DISTINCT CASE WHEN products_made_it = 1 THEN website_session_id ELSE NULL END) AS to_products
+	, COUNT(DISTINCT CASE WHEN mrfuzzy_made_it = 1 THEN website_session_id ELSE NULL END ) AS to_mrfuzzy
+	, COUNT(DISTINCT CASE WHEN cart_made_it = 1 THEN website_session_id ELSE NULL END) AS to_cart
+	, COUNT(DISTINCT CASE WHEN shipping_made_it = 1 THEN website_session_id ELSE NULL END) AS to_shipping
+	, COUNT(DISTINCT CASE WHEN billing_made_it = 1 THEN website_session_id ELSE NULL END) AS to_billing
+	, COUNT(DISTINCT CASE WHEN billing_made_it = 1 THEN website_session_id ELSE NULL END ) AS to_thank_you
+FROM #session_level_made_if_flagged
+GROUP BY CASE 
+		WHEN home_made_it = 1 THEN 'saw_homepage'
+		WHEN lander_made_it = 1 THEN 'saw_custom_lander'
+		ELSE 'check the logic '
+	 END
+GO
+
+SELECT 
+	CASE 
+		WHEN home_made_it = 1 THEN 'saw_homepage'
+		WHEN lander_made_it = 1 THEN 'saw_custom_lander'
+		ELSE 'check the logic '
+	END AS segment
+	, COUNT(DISTINCT website_session_id) AS sessions
+	, COUNT(DISTINCT CASE WHEN products_made_it = 1 THEN website_session_id ELSE NULL END)*1.0/COUNT(DISTINCT website_session_id) AS home_click_rt
+	, COUNT(DISTINCT CASE WHEN mrfuzzy_made_it = 1 THEN website_session_id ELSE NULL END )*1.0/COUNT(DISTINCT CASE WHEN products_made_it = 1 THEN website_session_id ELSE NULL END) AS product_click_rt
+	, COUNT(DISTINCT CASE WHEN cart_made_it = 1 THEN website_session_id ELSE NULL END)*1.0/COUNT(DISTINCT CASE WHEN mrfuzzy_made_it = 1 THEN website_session_id ELSE NULL END ) AS mrfuzzy_click_rt
+	, COUNT(DISTINCT CASE WHEN shipping_made_it = 1 THEN website_session_id ELSE NULL END)*1.0 / COUNT(DISTINCT CASE WHEN cart_made_it = 1 THEN website_session_id ELSE NULL END)AS card_click_rt
+	, COUNT(DISTINCT CASE WHEN billing_made_it = 1 THEN website_session_id ELSE NULL END)*1.0 / COUNT(DISTINCT CASE WHEN shipping_made_it = 1 THEN website_session_id ELSE NULL END) AS shipping_click_rt
+	, COUNT(DISTINCT CASE WHEN billing_made_it = 1 THEN website_session_id ELSE NULL END )*1.0 / COUNT(DISTINCT CASE WHEN billing_made_it = 1 THEN website_session_id ELSE NULL END) AS billing_click_rt
+FROM #session_level_made_if_flagged
+GROUP BY CASE 
+		WHEN home_made_it = 1 THEN 'saw_homepage'
+		WHEN lander_made_it = 1 THEN 'saw_custom_lander'
+		ELSE 'check the logic '
+	 END
+GO
